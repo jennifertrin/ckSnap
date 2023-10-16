51de2a343e13aef0a23bf68a905e3cc18ff17bf6
jennifertrin <jennifer.tran@dfinity.org>
2023-10-16 13:29:13 -0700

    fix: rerun dfx generate and error with saving values in form

--- src/components/ProposalForm.tsx
+++ src/components/ProposalForm.tsx
@@ -1,20 +1,19 @@
 import { useState } from "react";
-import { backend } from "../declarations/backend/index";
+import { backend } from "../declarations/backend";
 
 export default function ProposalForm() {
-
-  const [contractAddress, setContractAddress] = useState<string>('');
+  const [contractAddress, setContractAddress] = useState<string>("");
   const [tokenAmount, setTokenAmount] = useState<number>(0);
-  const [proposalTitle, setProposalTitle] = useState<string>('');
-  const [proposalDescription, setProposalDescription] = useState<string>('');
+  const [proposalTitle, setProposalTitle] = useState<string>("");
+  const [proposalDescription, setProposalDescription] = useState<string>("");
 
-  const profileToUpdate = {
+  const proposalToUpdate = {
     contract_address: contractAddress,
     amount: tokenAmount,
     title: proposalTitle,
     description: proposalDescription,
   };
-  
+
   const formVariables = [
     {
       label: "Contract Address",
@@ -37,14 +36,14 @@ export default function ProposalForm() {
       id: "proposalTitle",
       type: "text",
       value: proposalTitle,
-      setFunction: setProposalTitle
+      setFunction: setProposalTitle,
     },
     {
       label: "Proposal Description",
       id: "proposalDescription",
       type: "text",
       value: proposalDescription,
-      setFunction: setProposalDescription
+      setFunction: setProposalDescription,
     },
   ];
 
@@ -77,6 +76,11 @@ export default function ProposalForm() {
                       autoComplete={form.id}
                       className="border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 w-full"
                       placeholder={form.placeholder}
+                      value={form.value}
+                      onChange={(e) => {
+                        const inputValue = form.type === 'number' ? parseFloat(e.target.value) : e.target.value;
+                        form.setFunction(inputValue as any);
+                      }}
                     />
                   </div>
                 </div>
@@ -92,7 +96,12 @@ export default function ProposalForm() {
         >
           Cancel
         </button>
-        <button onClick={() => backend.update(profileToUpdate)} className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+        <button
+          onClick={async () => {
+            await backend.update(proposalToUpdate);
+          }}
+          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
           Save
         </button>
       </div>
