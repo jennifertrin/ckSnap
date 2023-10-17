d7a5f171556341a6d0a1c32c30956bc679b6f717
jennifertrin <jennifer.tran@dfinity.org>
2023-10-16 23:30:58 -0700

    add Wallet Connect button

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,33 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/react'
+
+import { WagmiConfig } from 'wagmi'
+import { arbitrum, mainnet } from 'wagmi/chains'
+
+const projectId = 'YOUR_PROJECT_ID'
+
+const metadata = {
+  name: 'Web3Modal',
+  description: 'Web3Modal Example',
+  url: 'https://web3modal.com',
+  icons: ['https://avatars.githubusercontent.com/u/37784886']
+}
+
+const chains = [mainnet, arbitrum]
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata })
+
+createWeb3Modal({ wagmiConfig, projectId, chains })
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <WagmiConfig config={wagmiConfig}>
     <App />
+    </WagmiConfig>
   </React.StrictMode>
 );
 
