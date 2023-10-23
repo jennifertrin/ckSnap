f23321049cafbb654552dd7e85db9b661d67089a
Jennifer K. Tran <38402540+jennifertrin@users.noreply.github.com>
2023-10-23 16:31:08 -0700

    style: add expiration and execution ui (#8)

--- src/components/MainCTA.tsx
+++ src/components/MainCTA.tsx
@@ -8,22 +8,9 @@ export default function MainCTA() {
   const [proposalListSection, setProposalListSection] = useState<boolean>(false);
 
   return (
-    <div className="bg-white">
+    <div className="bg-[radial-gradient(ellipse_at_left,_var(--tw-gradient-stops))] from-indigo-400 via-indigo-300 to-fuchsia-100 h-screen">
       <div className="flex flex-col lg:flex-row">
-        <div className="flex relative isolate px-6 pt-14 lg:px-8 w-full sm:w-3/5">
-          <div
-            className="absolute transform-gpu overflow-hidden blur-3xl"
-            aria-hidden="true"
-          >
-            <div
-              className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-              style={{
-                clipPath:
-                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-              }}
-            />
-          </div>
-          <div className={`m-auto -mt-4 max-w-2xl py-48 lg:py-56`}>
+      <div className={`m-auto -mt-4 max-w-2xl py-48 lg:py-56`}>
             <div className="text-center">
               <h1 className={`text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl`}>
                 Welcome to ckSnap
@@ -51,19 +38,6 @@ export default function MainCTA() {
               </div>
             </div>
           </div>
-          <div
-            className="absolute transform-gpu overflow-hidden blur-3xl"
-            aria-hidden="true"
-          >
-            <div
-              className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-              style={{
-                clipPath:
-                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-              }}
-            />
-          </div>
-        </div>
         <div className="flex mx-auto w-full lg:w-2/5">
           {proposalSection ? <ProposalForm></ProposalForm> : null}
           {proposalListSection ? <ProposalList></ProposalList> : null}
--- src/declarations/backend/backend.did.d.ts
+++ src/declarations/backend/backend.did.d.ts
@@ -3,13 +3,51 @@ import type { ActorMethod } from '@dfinity/agent';
 
 export interface _SERVICE {
   'createProposal' : ActorMethod<
-    [string, string, number, string, string],
+    [
+      string,
+      string,
+      number,
+      string,
+      string,
+      number,
+      {
+        'token_amount' : number,
+        'to_address' : string,
+        'from_address' : string,
+      },
+    ],
     {
       'id' : string,
       'title' : string,
       'description' : string,
+      'deadline' : number,
+      'execution' : {
+        'token_amount' : number,
+        'to_address' : string,
+        'from_address' : string,
+      },
+      'block' : string,
       'contract_address' : string,
       'amount' : number,
+    }
+  >,
+  'ethGetBalance' : ActorMethod<[string], string>,
+  'ethGetBlockByNumber' : ActorMethod<[number], string>,
+  'ethTransform' : ActorMethod<
+    [
+      {
+        'context' : Uint8Array | number[],
+        'response' : {
+          'status' : bigint,
+          'body' : Uint8Array | number[],
+          'headers' : Array<{ 'value' : string, 'name' : string }>,
+        },
+      },
+    ],
+    {
+      'status' : bigint,
+      'body' : Uint8Array | number[],
+      'headers' : Array<{ 'value' : string, 'name' : string }>,
     }
   >,
   'getAllProposals' : ActorMethod<
@@ -19,6 +57,13 @@ export interface _SERVICE {
         'id' : string,
         'title' : string,
         'description' : string,
+        'deadline' : number,
+        'execution' : {
+          'token_amount' : number,
+          'to_address' : string,
+          'from_address' : string,
+        },
+        'block' : string,
         'contract_address' : string,
         'amount' : number,
       }
@@ -31,9 +76,21 @@ export interface _SERVICE {
         'id' : string,
         'title' : string,
         'description' : string,
+        'deadline' : number,
+        'execution' : {
+          'token_amount' : number,
+          'to_address' : string,
+          'from_address' : string,
+        },
+        'block' : string,
         'contract_address' : string,
         'amount' : number,
       }
     ]
+  >,
+  'publicKey' : ActorMethod<[], { 'publicKey' : Uint8Array | number[] }>,
+  'sign' : ActorMethod<
+    [Uint8Array | number[]],
+    { 'signature' : Uint8Array | number[] }
   >,
 }
