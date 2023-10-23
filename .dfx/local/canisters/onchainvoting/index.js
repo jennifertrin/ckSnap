3293e332399e6369a9f39249359ea6dd7ed5ca5e
jennifertrin <jennifer.tran@dfinity.org>
2023-10-23 16:41:04 -0700

    chore: adjust env variables

--- .dfx/local/canisters/onchainvoting/index.js
+++ .dfx/local/canisters/onchainvoting/index.js
@@ -4,7 +4,7 @@ import { Actor, HttpAgent } from "@dfinity/agent";
 import { idlFactory } from './onchainvoting.did.js';
 export { idlFactory } from './onchainvoting.did.js';
 // CANISTER_ID is replaced by webpack based on node environment
-export const canisterId = process.env.ONCHAINVOTING_CANISTER_ID;
+export const canisterId = process.env.REACT_APP_ONCHAINVOTING_CANISTER_ID;
 
 /**
  * @deprecated since dfx 0.11.1
