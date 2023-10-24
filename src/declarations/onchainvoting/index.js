b120a13074a50274c7c7da64040f5d6099465f59
jennifertrin <jennifer.tran@dfinity.org>
2023-10-24 00:23:19 -0700

    fix: append REACT_APP_ to env variables

--- src/declarations/onchainvoting/index.js
+++ src/declarations/onchainvoting/index.js
@@ -10,8 +10,8 @@ export { idlFactory } from "./onchainvoting.did.js";
  * beginning in dfx 0.15.0
  */
 export const canisterId =
-  process.env.CANISTER_ID_ONCHAINVOTING ||
-  process.env.ONCHAINVOTING_CANISTER_ID;
+  process.env.REACT_APP_CANISTER_ID_ONCHAINVOTING ||
+  process.env.REACT_APP_ONCHAINVOTING_CANISTER_ID;
 
 export const createActor = (canisterId, options = {}) => {
   const agent = options.agent || new HttpAgent({ ...options.agentOptions });
